fa516bf80fff944aa89e634cdc4b4851d0884748
Christine Coulter <christinetcoulter@gmail.com>
2015-01-19 18:59:20 -0500

    Edit routes so that root is where the user will login.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'welcome#index'
+  devise_for :users, skip: [:sessions]
+  as :user do
+    root to: 'devise/sessions#new', as: :new_user_session
+    post '/' => 'devise/sessions#create', as: :user_session
+    delete 'signout' => 'devise/sessions#destroy', as: :destroy_user_session
+  end
 end
