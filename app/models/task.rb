6d184e14bda2da3059e72fb89dc950aeb3186b9e
Christine Coulter <christinetcoulter@gmail.com>
2015-01-19 23:38:04 -0500

    Write Task.make to create a new task for the user.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,10 @@
 class Task < ActiveRecord::Base
   belongs_to :user
+
+  def self.make(user_id, params)
+    task = Task.new(params)
+    task.user_id = user_id
+    task.save
+    task
+  end
 end
