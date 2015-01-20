36cc1de6feef52f80f54f8049db384f3b6a34298
Christine Coulter <christinetcoulter@gmail.com>
2015-01-20 00:59:07 -0500

    Write edit and update actions in TasksController.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,6 +8,15 @@ class TasksController < ApplicationController
     redirect_to root_path
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    Task.find_and_update(params[:id], task_params)
+    redirect_to root_path
+  end
+
 private
 
   def task_params
