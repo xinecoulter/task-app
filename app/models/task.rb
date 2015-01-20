6540327921155fb40bd7f2bc85cb2ebea8ec2eea
Christine Coulter <christinetcoulter@gmail.com>
2015-01-20 00:34:26 -0500

    Write Task.calculate_interval to caclulate the number of seconds given the number of days/weeks/months.

--- app/models/task.rb
+++ app/models/task.rb
@@ -9,4 +9,16 @@ class Task < ActiveRecord::Base
     task.save
     task
   end
+
+  def self.calculate_interval(interval_number, interval_type)
+    case interval_type
+    when "days"
+      seconds_multiplier = 86400 # 1 day (24 hrs * 60 min * 60 sec)
+    when "weeks"
+      seconds_multiplier = 604800 # 1 week (7 days * 24 hrs * 60 min * 60 sec)
+    when "months"
+      seconds_multiplier = 2592000 # 30 days (30 days * 24 hrs * 60 min * 60 sec)
+    end
+    interval_number * seconds_multiplier
+  end
 end
