30a11455e1ae77087ad99da69074cac93c602450
Christine Coulter <christinetcoulter@gmail.com>
2015-01-19 23:41:24 -0500

    Edit root path.

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   end
 
   as :user do
-    root to: 'devise/sessions#new', as: :new_user_session
+    root to: 'devise/sessions#new'
     post '/' => 'devise/sessions#create', as: :user_session
     delete 'signout' => 'devise/sessions#destroy', as: :destroy_user_session
   end
