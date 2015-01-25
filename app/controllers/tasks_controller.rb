af8a7315510ebbeb0d11dad500e104833624e7c6
Christine Coulter <christinetcoulter@gmail.com>
2015-01-25 02:39:05 -0500

    Add last_completed_at in strong params for tasks.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -47,6 +47,6 @@ class TasksController < ApplicationController
 private
 
   def task_params
-    params.require(:task).permit(:name, :description, :last_completed_at, :interval_number, :interval_type)
+    params.require(:task).permit(:name, :description, :last_completed_at, :interval_number, :interval_type, :last_completed_at)
   end
 end
