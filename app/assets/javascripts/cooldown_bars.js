12ab08f7d8e44ff1c695b42f6e4276354d7b2a6b
Christine Coulter <christinetcoulter@gmail.com>
2015-02-02 00:59:48 -0500

    Javascript for progress bars with text that updates with remaining time left until task is due.

--- app/assets/javascripts/cooldown_bars.js
+++ app/assets/javascripts/cooldown_bars.js
@@ -3,10 +3,18 @@ window.CooldownBars = {};
 CooldownBars.setupBar = function(bar) {
     var interval = bar.data("interval") * 1000; // convert to milliseconds
     var dueDate = bar.data("due-date") * 1000; // convert to milliseconds
+    var progressLabel = bar.find( ".progress-label" );
 
     bar.progressbar({
         value: ( dueDate - new Date($.now()) ),
-        max: interval
+        max: interval,
+        change: function() {
+            if ( bar.progressbar("value") === 0 ) {
+                bar.text( bar.data("task-name") + ": DUE NOW!" );
+            } else {
+                progressLabel.text( bar.data("task-name") + ": " + CooldownBars.timeLeft( bar.progressbar( "value" )) + " remaining" );
+            }
+        }
     });
 
     function progress() {
@@ -17,6 +25,28 @@ CooldownBars.setupBar = function(bar) {
         }
     }
     setTimeout(progress, 0);
+};
+
+CooldownBars.timeLeft = function(milliseconds) {
+    var totalSeconds = Math.floor(milliseconds / 1000); // convert to seconds
+    var days = Math.floor(totalSeconds / 86400); // 1 day ( 24 hrs * 60 min * 60 sec ) = 86400 sec
+    var hours = Math.floor((totalSeconds %= 86400) / 3600); // 1 hr ( 60 min * 60 sec ) = 3600 sec
+    var minutes = Math.floor((totalSeconds %= 3600) / 60); // 1 min = 60 sec
+    var seconds = totalSeconds % 60;
+    var secondsText = seconds + " second" + numberEnding(seconds);
+    var minutesText = minutes + " minute" + numberEnding(minutes) + " and " + secondsText;
+    var hoursText = hours + " hour" + numberEnding(hours) + ", " + minutesText;
+    var daysText = days + " day" + numberEnding(days) + ", " + hoursText;
+
+    function numberEnding(number) {
+        return (number > 1) ? "s" : "";
+    }
+
+    if (days) return daysText;
+    if (hours) return hoursText;
+    if (minutes) return minutesText;
+    if (seconds) return secondsText;
+    return "less than a second";
 };
 
 $(function() {
