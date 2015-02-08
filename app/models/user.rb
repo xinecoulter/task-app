98021dd126c7c6957d5298a292199e9b892cb621
Christine Coulter <christinetcoulter@gmail.com>
2015-02-07 22:03:23 -0500

    Define relationships with Team and TeamMembership for User.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :tasks
+  has_many :team_memberships, foreign_key: :member_id, dependent: :destroy
+  has_many :teams, through: :team_memberships
 end
