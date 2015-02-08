5bf76d13a0e9fa07fb4c4b221eefdcac16ada252
Christine Coulter <christinetcoulter@gmail.com>
2015-02-08 18:34:00 -0500

    Write Team.make.

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,4 +3,13 @@ class Team < ActiveRecord::Base
   has_many :members, through: :team_memberships
 
   validates_presence_of :name
+
+  resourcify
+
+  def self.make(owner, params)
+    team = new(params)
+    team.save
+    owner.add_role :owner, team
+    team
+  end
 end
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -12,4 +12,25 @@ describe Team do
     team = build(:team, name: nil)
     expect { team.save! }.to raise_error
   end
+
+  describe ".make" do
+    let(:user) { create(:user) }
+    let(:name) { "Big Hero 6" }
+    let(:params) { { name: name } }
+    subject { Team.make(user, params) }
+
+    it "makes a new team" do
+      expect{ subject }.to change(Team, :count).by(1)
+    end
+
+    it "gives the team the specified attributes" do
+      team = subject
+      assert(team.name == name)
+    end
+
+    it "makes the user the owner of the team" do
+      team = subject
+      assert(user.has_role? :owner, team)
+    end
+  end
 end
