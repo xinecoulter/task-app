2dbf40098887ff8ce01cb2b6cb8a362a24894ec6
Christine Coulter <christinetcoulter@gmail.com>
2015-02-07 19:35:24 -0500

    Create teams and team_memberships tables.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150126014235) do
+ActiveRecord::Schema.define(version: 20150208003049) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,19 @@ ActiveRecord::Schema.define(version: 20150126014235) do
     t.integer  "interval_number"
     t.string   "interval_type"
     t.integer  "task_icon_id"
+  end
+
+  create_table "team_memberships", force: true do |t|
+    t.integer  "team_id"
+    t.integer  "member_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "teams", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
