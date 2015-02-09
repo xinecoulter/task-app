fe3aabe2fc79ef594f1fe5933c193f2cf55c94cb
Christine Coulter <christinetcoulter@gmail.com>
2015-02-09 01:03:03 -0500

    Write new and create actions for TeamsController. Basic html for teams new view and form partial.

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,4 +2,28 @@ class TeamsController < ApplicationController
   def index
     @teams = current_user.teams
   end
+
+  def new
+    @team = Team.new
+    authorize! :create, @team
+  end
+
+  def create
+    @team = authorize_with_transaction!(:create) do
+      Team.make(current_user, team_params)
+    end
+    if @team.valid?
+      flash[:notice] = "Awesomesauce! Team #{@team.name} successfully created."
+      redirect_to team_path(@team)
+    else
+      flash[:error] = "Do not pass Go. Do not collect $200. JK, change something and try it again."
+      render :new
+    end
+  end
+
+private
+
+  def team_params
+    params.require(:team).permit(:name)
+  end
 end
--- spec/controllers/teams_controller_spec.rb
+++ spec/controllers/teams_controller_spec.rb
@@ -20,4 +20,67 @@ describe TeamsController do
       assert(user.teams == assigns[:teams])
     end
   end
+
+  describe "GET 'new'" do
+    subject { get :new }
+
+    it "checks authorization" do
+      new_team = build(:team)
+      Team.stub(:new) { new_team }
+      controller.should_receive(:authorize!).with(:create, new_team)
+      subject
+    end
+
+    it "renders the :new template" do
+      subject
+      expect(response).to render_template :new
+    end
+  end
+
+  describe "POST 'create'" do
+    let(:the_team) { create(:team) }
+    let(:name) { "Guardians of the Galaxy" }
+    let(:params) { { name: name } }
+    subject { post :create, team: params }
+
+    it "checks authorization" do
+      Team.stub(:make) { the_team }
+      controller.should_receive(:authorize!).with(:create, the_team)
+      subject
+    end
+
+    context "with valid attributes" do
+      it "saves the new team in the database" do
+        expect { subject }.to change(Team, :count).by(1)
+      end
+
+      it "sets the flash" do
+        subject
+        assert("Awesomesauce! Team #{name} successfully created." == flash[:notice])
+      end
+
+      it "redirects to the team" do
+        subject
+        assert_redirected_to team_path(assigns[:team])
+      end
+    end
+
+    context "with invalid attributes" do
+      let(:name) { "" }
+
+      it "does not save the new team in the database" do
+        expect { subject }.to_not change(Team, :count)
+      end
+
+      it "sets the flash" do
+        subject
+        assert("Do not pass Go. Do not collect $200. JK, change something and try it again." == flash[:error])
+      end
+
+      it "re-renders the :new template" do
+        subject
+        expect(response).to render_template :new
+      end
+    end
+  end
 end
