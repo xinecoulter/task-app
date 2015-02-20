d5427efdac286634c6d92de9bf6fddee1597f965
Christine Coulter <christinetcoulter@gmail.com>
2015-02-19 22:34:46 -0500

    Add config settings for being able to filter specs by the focus tag.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -49,4 +49,7 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
   config.include FactoryGirl::Syntax::Methods
+
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
 end
