2541e8711346fa16debd3f74117b067401650952
Christine Coulter <christinetcoulter@gmail.com>
2015-07-15 23:11:00 -0400

    Config for putting focus on specs.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -82,4 +82,6 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
 end
