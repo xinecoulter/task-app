d4c297aabdb71352c584c90f567f2c9e58925942
Christine Coulter <christinetcoulter@gmail.com>
2015-07-19 00:30:58 -0400

    Omniauth config.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable
 
   has_many :tasks, dependent: :destroy
   has_many :team_memberships, foreign_key: :member_id, dependent: :destroy
